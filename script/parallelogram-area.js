7be1297e6761980f85849a08607f33db1252310a
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-02-18 00:04:32 +0600

    added parallelogram

---
+++ script/parallelogram-area.js
@@ -0,0 +1,25 @@
+function calculateParallelogramArea(){
+    const base = getInputValueById('parallelogram-base');
+    // console.log('base value', base);
+
+    const height = getInputValueById('parallelogram-height');
+    // console.log('height value', height);
+    const area = base * height;
+    console.log('parallelogram area', area);
+
+    setInnerTextById('parallelogram-area', area);
+
+
+}
+
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    return inputValue;
+}
+
+function setInnerTextById(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
